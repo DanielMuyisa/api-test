dfa9f09b60e7d4da16be94852c70c57d65165b56
danielmuyisa <danielmuyisa37@gmail.com>
2023-06-09 19:53:54 +0100

    init

---
+++ php/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> Test api </h1>
+    <hr>
+
+    <h5> REGISTER PAYMENT </h5>
+    <input type="text" name="Label" id=""><br />
+    <input type="number" name="amount" id=""><br />
+    <button type="submit"> Valid </button>
+    
+</body>
+</html>
